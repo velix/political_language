ef41f703f3d3659efc4ce3f202b2323b2589977c
velix <miloulis.v@gmail.com>
2018-12-14 00:10:58 +0100

    manual epoch training

--- src/hierarchical_classification.py
+++ src/hierarchical_classification.py
@@ -7,13 +7,14 @@ dl = DataLoader.DataLoader()
 
 model = Sequential()
 
-model.add(Bidirectional(GRU(units=50)), input_shape=(1, 384))
+model.add(Bidirectional(GRU(units=50), input_shape=(None, 768)))
 model.add(Dense(1, activation="softmax"))
 model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 
-hist = model.fit_generator(dl.generate_vectors(DataLoader.TRAINING_DATA_DIR),
-                           validation_data=dl.generate_vectors(DataLoader.DEV_DATA_DIR),
-                           epochs=20, steps_per_epoch=len(dl.generate_vectors(DataLoader.TRAINING_DATA_DIR)))
+for epoch in range(20):
+    for (X, y) in dl.generate_vectors(DataLoader.TRAINING_DATA_DIR):
+        (X_dev, y_dev) = next(dl.generate_vectors(DataLoader.DEV_DATA_DIR))
+        hist = model.fit(X, y, batch_size=len(X), epochs=1)
 
 score = model.evaluate_generator(dl.generate_vectors(DataLoader.DEV_DATA_DIR))
 
