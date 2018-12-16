eb304948140c751c59c29114ccd528d95d3a4a1a
velix <miloulis.v@gmail.com>
2018-12-17 00:25:19 +0100

    attention redux

--- src/Attention.py
+++ src/Attention.py
@@ -1,44 +1,73 @@
-from keras.engine.topology import Layer
-from keras import initializations
 from keras import backend as K
+from keras.engine.topology import Layer
+from keras import initializers, regularizers, constraints
 
 
 class Attention(Layer):
-    '''Attention operation for temporal data.
-    # Input shape
-        3D tensor with shape: `(samples, steps, features)`.
-    # Output shape
-        2D tensor with shape: `(samples, features)`.
-    '''
-    def __init__(self, attention_dim, **kwargs):
-        self.init = initializations.get('glorot_uniform')
-        self.attention_dim = attention_dim
+    def __init__(self, step_dim,
+                 W_regularizer=None, b_regularizer=None,
+                 W_constraint=None, b_constraint=None,
+                 bias=True, **kwargs):
+        self.supports_masking = True
+        self.init = initializers.get('glorot_uniform')
+
+        self.W_regularizer = regularizers.get(W_regularizer)
+        self.b_regularizer = regularizers.get(b_regularizer)
+
+        self.W_constraint = constraints.get(W_constraint)
+        self.b_constraint = constraints.get(b_constraint)
+
+        self.bias = bias
+        self.step_dim = step_dim
+        self.features_dim = 0
         super(Attention, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        self.W = self.init((self.attention_dim, self.attention_dim),
-                           name='{}_W'.format(self.name))
-        self.b = K.zeros((self.attention_dim,), name='{}_b'.format(self.name))
-        self.u = self.init((self.attention_dim,), name='{}_u'.format(self.name))
-        self.trainable_weights += [self.W, self.b, self.u]
+        assert len(input_shape) == 3
+
+        self.W = self.add_weight((input_shape[-1],),
+                                 initializer=self.init,
+                                 name='{}_W'.format(self.name),
+                                 regularizer=self.W_regularizer,
+                                 constraint=self.W_constraint)
+        self.features_dim = input_shape[-1]
+
+        if self.bias:
+            self.b = self.add_weight((input_shape[1],),
+                                     initializer='zero',
+                                     name='{}_b'.format(self.name),
+                                     regularizer=self.b_regularizer,
+                                     constraint=self.b_constraint)
+        else:
+            self.b = None
+
         self.built = True
 
-    def get_output_shape_for(self, input_shape):
-        return (input_shape[0], input_shape[2])
+    def compute_mask(self, input, input_mask=None):
+        return None
 
     def call(self, x, mask=None):
-        # Calculate the first hidden activations
-        a1 = K.tanh(K.dot(x, self.W) + self.b)  # [n_samples, n_steps, n_hidden]
-        # K.dot won't let us dot a 3D with a 1D so we do it with mult + sum
-        mul_a1_u = a1 * self.u                  # [n_samples, n_steps, n_hidden]
-        dot_a1_u = K.sum(mul_a1_u, axis=2)      # [n_samples, n_steps]
-        # Calculate the per step attention weights
-        a2_num = K.exp(dot_a1_u)                # [n_samples, n_steps]
-        a2_den = K.sum(a2_num, axis=1)          # [n_samples]
-        a2_den = K.expand_dims(a2_den)          # [n_samples, 1] so div broadcasts
-        a2 = a2_num / a2_den                    # [n_samples, n_steps]
-        a2 = K.expand_dims(a2)                  # [n_samples, n_steps, 1] so div broadcasts
-        # Apply attention weights to steps
-        weighted_input = x * a2                 # [n_samples, n_steps, n_features]
-        # Sum across the weighted steps to get the pooled activations
+        features_dim = self.features_dim
+        step_dim = self.step_dim
+
+        eij = K.reshape(K.dot(K.reshape(x, (-1, features_dim)),
+                        K.reshape(self.W, (features_dim, 1))), (-1, step_dim))
+
+        if self.bias:
+            eij += self.b
+
+        eij = K.tanh(eij)
+
+        a = K.exp(eij)
+
+        if mask is not None:
+            a *= K.cast(mask, K.floatx())
+
+        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+
+        a = K.expand_dims(a)
+        weighted_input = x * a
         return K.sum(weighted_input, axis=1)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0],  self.features_dim
